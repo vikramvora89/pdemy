a466b41c9ccc16754aa66c296e3d23549c3c91be
vikramvora89 <82162263+vikramvora89@users.noreply.github.com>
2021-09-23 22:22:03 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withPWA = require('next-pwa')
 
 module.exports = withPWA({
     pwa: {
-        disable: process.env.NODE_ENV === 'development',
+        //disable: process.env.NODE_ENV === 'development',
         // dest: 'public',
         register: true,
         sw: '/sw.js'
